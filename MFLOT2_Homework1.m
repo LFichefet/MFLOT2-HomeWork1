0198a4c170cfec741cea520ec40b7fe4540c2ec3
LFichefet <44224231+LFichefet@users.noreply.github.com>
2018-10-18 14:41:05 +0200

    Update MFLOT2_Homework1.m
    Petits changements mineurs + le plot (qui est bien moins sexy que sur l'image du homework !)

--- MFLOT2_Homework1.m
+++ MFLOT2_Homework1.m
@@ -1,4 +1,4 @@
-%Donnée:
+%Donnees:
 b = 50e-3; % largeur de la conduite [m]
 d_e = 15e-3; % hauteur de la sortie de la conduite [m]
 h_e = 0.5*d_e; % [m]
@@ -11,7 +11,7 @@ alpha = 3.25*pi/180; % angle de pente divergente [rad]
 r1 = 254.3e-3; % rayon de courbure de la gorge [m] 
 r2 = 153.7e-3; % rayon de courbure fin de divergente [m]
 X = linspace(0,0.39,4000);
-H = zeros(1,1000);
+H = zeros(1,4000);
 X_p1 = L_c + r1/( sqrt( 1/(tan(alpha))^2 -1 ) );
 
 X_p2 = L_c + L_d - r2/( sqrt( 1/(tan(alpha))^2 +1 ) );
@@ -20,7 +20,7 @@ for i=1:length(X)
     if X(i)<=X_p1
         H(i) = h_t + r1 - sqrt( (r1)^2 - (X(i) - L_c)^2 );
     elseif X(i)<=X_p2 && X(i)>X_p1
-        H(i) = tan(alpha)*X(i) + 9.335e-4;
+        H(i) = tan(alpha)*X(i) + 9.15e-4;
     elseif X(i)<=0.12 && X(i)>X_p2
         H(i) = h_e - r2 + sqrt( (r2)^2 - (X(i) -L_c -L_d)^2);
     else
